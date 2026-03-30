6d7aebb190a95c57ce4a2741579cd050a46fde78
Riley36666 <Riley.36666@gmail.com>
2026-03-30 09:12:49 +0100

    Update before testing Claude code

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
+import "./Navbar.css";
 
 export default function Navbar() {
   const { pathname } = useLocation();
