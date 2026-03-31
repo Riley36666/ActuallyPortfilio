8248ce976f93f523f0f3dc4998b48c63414ed7e9
Riley36666 <Riley.36666@gmail.com>
2026-03-31 22:09:04 +0100

    fixing stuff

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -28,6 +28,11 @@ app.use("/admin", adminRoute);
 
 
 app.use(express.static(buildPath));
+app.use(express.static("dist"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
 
 
 
